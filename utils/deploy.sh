f1abab6749bc7aab7fddfcffd5484815ba880570
infu <vtsukev@gmail.com>
2024-11-25 14:59:20 +0200

    pylon val adjustment

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -6,7 +6,7 @@ record {
      PYLON_GOVERNED_BY = \"Neutrinite DAO\";
      BILLING = record {
          ledger = principal \"f54if-eqaaa-aaaaq-aacea-cai\";
-         min_create_balance = 3000000;
+         min_create_balance = 50000000;
          operation_cost = 1000;
          freezing_threshold_days = 10;
          exempt_daily_cost_balance = null;
@@ -28,7 +28,7 @@ record {
      TEMP_NODE_EXPIRATION_SEC = 3600;
      MAX_INSTRUCTIONS_PER_HEARTBEAT = 300_000_000;
      REQUEST_MAX_EXPIRE_SEC = 3600;
-     ALLOW_TEMP_NODE_CREATION = true;
+     ALLOW_TEMP_NODE_CREATION = false;
  }
 )"
 
